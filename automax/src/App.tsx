e369ccb86c1f0c312712e57097af3aa220025b0d
Lucas Bueno <gelanlucas@gmail.com>
2023-04-12 12:12:26 -0300

    feat/navbar

--- automax/src/App.tsx
+++ automax/src/App.tsx
@@ -1,11 +1,15 @@
 
 import { GlobalStyle } from "./style/globalStyles";
 import Routes from "./routes";
+import { NavBar } from "./components/NavBar";
+import { Footer } from "./components/footer";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <NavBar />
+      <Footer />
       <Routes />
     </>
   );
