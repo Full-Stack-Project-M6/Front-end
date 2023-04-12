e8dc8ebc2d2dcd9ac7efc4eaefc702d8743b7302
M7uricio <106341669+M7uricio@users.noreply.github.com>
2023-04-12 13:04:20 -0300

    Update App.tsx
    remove components from app file

--- automax/src/App.tsx
+++ automax/src/App.tsx
@@ -1,15 +1,11 @@
 
 import { GlobalStyle } from "./style/globalStyles";
 import Routes from "./routes";
-import { NavBar } from "./components/NavBar";
-import { Footer } from "./components/footer";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <NavBar />
-      <Footer />
       <Routes />
     </>
   );
