f4c9836ac225f2de6a8504c57fa87068b4fda51c
Lucas Bueno <gelanlucas@gmail.com>
2023-04-12 12:10:15 -0300

    feat/add-mobile-first

--- automax/src/components/footer/index.tsx
+++ automax/src/components/footer/index.tsx
@@ -1,10 +1,11 @@
 import { StyledFooter } from "./style"
 import { BsFillCaretUpFill } from 'react-icons/bs'
+import whitelogo from '../../assets/whiteLogo.png'
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <h2>AutoMax</h2>
+            <img src={whitelogo} alt="logo" />
             <p>© 2023 -  Todos os direitos reservados.</p>
             <button><BsFillCaretUpFill /></button>
         </StyledFooter>
