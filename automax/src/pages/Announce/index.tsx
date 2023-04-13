0d48d57070a2a3ac71da73c20bbd42fd99447725
M7uricio <m7uricio.dev@gmail.com>
2023-04-13 09:39:44 -0300

    feat:adds desck styling

--- automax/src/pages/Announce/index.tsx
+++ automax/src/pages/Announce/index.tsx
@@ -1,106 +1,161 @@
+import { NavBar } from "../../components/NavBar";
 import { StyleAnnoucePage } from "./style";
 import { Body1, Body2, H5, H6 } from "../../components/Typography";
 import Button from "../../components/Button";
 import Comment from "../../components/Coments/index";
 import Announcer from "../../components/Announcer";
 import { annnounceMocked } from "../../utils";
+import { Footer } from "../../components/footer";
 
 export const Announce = () => {
   return (
-    <StyleAnnoucePage>
-      <div className="backGround"></div>
-      <div className="divImgCard">
-        <img src={annnounceMocked.imgCar} alt="" />
-      </div>
-      <div className="divInfosCar">
-        <H6>Mercedes Benz A 200 CGI ADVANCE SEDAN</H6>
-        <div className="divInfoNumbers">
-          <div>
-            <Body2>2013</Body2>
-            <Body2>0 KM</Body2>
+    <>
+      <NavBar />
+      <StyleAnnoucePage>
+        <div className="backGround" />
+        <div className="body">
+          <div className="divImgCard">
+            <img src={annnounceMocked.imgCar} alt="" />
           </div>
-          <div>
-            <Body1 weight={600}>R$ 00.000,00</Body1>
-          </div>
-        </div>
-        <div>
-          <Button className="brand1">Comprar</Button>
-        </div>
-      </div>
-      <div className="divDescription">
-        <H6>Descrição</H6>
-        <Body1>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </Body1>
-      </div>
-      <div>
-        <div className="divPhotos">
-          <div>
-            <H6>Fotos</H6>
-          </div>
-          <div className="divImgs">
-            <div>
+          <div className="divInfosCar">
+            <H6>Mercedes Benz A 200 CGI ADVANCE SEDAN</H6>
+            <div className="divInfoNumbers">
               <div>
-                <img src={annnounceMocked.imgCar} alt="" />
+                <Body2>2013</Body2>
+                <Body2>0 KM</Body2>
               </div>
               <div>
-                <img src={annnounceMocked.imgCar} alt="" />
-              </div>
-              <div>
-                <img src={annnounceMocked.imgCar} alt="" />
+                <Body1 weight={600}>R$ 00.000,00</Body1>
               </div>
             </div>
             <div>
+              <Button className="brand1">Comprar</Button>
+            </div>
+          </div>
+          <div className="divDescription">
+            <H6>Descrição</H6>
+            <Body1>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </Body1>
+          </div>
+          <div>
+            <div className="divPhotos">
               <div>
-                <img src={annnounceMocked.imgCar} alt="" />
+                <H6>Fotos</H6>
               </div>
-              <div>
-                <img src={annnounceMocked.imgCar} alt="" />
-              </div>
-              <div>
-                <img src={annnounceMocked.imgCar} alt="" />
+              <div className="divImgs">
+                <div>
+                  <div>
+                    <img src={annnounceMocked.imgCar} alt="" />
+                  </div>
+                  <div>
+                    <img src={annnounceMocked.imgCar} alt="" />
+                  </div>
+                  <div>
+                    <img src={annnounceMocked.imgCar} alt="" />
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <img src={annnounceMocked.imgCar} alt="" />
+                  </div>
+                  <div>
+                    <img src={annnounceMocked.imgCar} alt="" />
+                  </div>
+                  <div>
+                    <img src={annnounceMocked.imgCar} alt="" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="divAnnoucer">
-        <div className="divTagName">
-          <div>
-            <H5 weight={500}>SL</H5>
+          <div className="divAnnoucer">
+            <div className="divTagName">
+              <div>
+                <H5 weight={500}>SL</H5>
+              </div>
+              <H6>Samuel Leão</H6>
+            </div>
+            <Body1>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's
+            </Body1>
+            <Button className="grey1">Ver todos anuncios</Button>
           </div>
-          <H6>Samuel Leão</H6>
-        </div>
-        <Body1>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's
-        </Body1>
-        <Button className="grey1">Ver todos anuncios</Button>
-      </div>
-      <div className="divComments">
-        <div>
-          <H6>Comentários</H6>
-        </div>
-        <Comment />
-        <Comment />
-        <Comment />
-      </div>
+          <div className="divComments">
+            <div>
+              <H6>Comentários</H6>
+            </div>
+            <Comment />
+            <Comment />
+            <Comment />
+          </div>
 
-      <div className="divComment">
-        <div>
-          <Announcer />
+          <div className="divComment">
+            <div>
+              <Announcer />
+            </div>
+            <div className="divInputButton">
+              <input type="text" className="inputComment" />
+              <Button className="brand1">Comentar</Button>
+            </div>
+
+            <div className="divDirt">
+              <p>Gostei muito</p>
+              <p>Incrível</p>
+              <p>Recomendo para emus amigos</p>
+            </div>
+          </div>
         </div>
-        <input type="text" />
-        <Button className="brand1">Comentar</Button>
-        <div className="divDirt">
-          <p>Gostei muito</p>
-          <p>Incrível</p>
-          <p>Recomendo para emus amigos</p>
-        </div>
-      </div>
-    </StyleAnnoucePage>
+        <aside className="aside">
+          <div className="divPhotosDesck">
+            <div>
+              <H6>Fotos</H6>
+            </div>
+            <div className="divImgs">
+              <div>
+                <div>
+                  <img src={annnounceMocked.imgCar} alt="" />
+                </div>
+                <div>
+                  <img src={annnounceMocked.imgCar} alt="" />
+                </div>
+                <div>
+                  <img src={annnounceMocked.imgCar} alt="" />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <img src={annnounceMocked.imgCar} alt="" />
+                </div>
+                <div>
+                  <img src={annnounceMocked.imgCar} alt="" />
+                </div>
+                <div>
+                  <img src={annnounceMocked.imgCar} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="divAnnoucerDesck">
+            <div className="divTagName">
+              <div>
+                <H5 weight={500}>SL</H5>
+              </div>
+              <H6>Samuel Leão</H6>
+            </div>
+            <Body1>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's
+            </Body1>
+            <Button className="grey1">Ver todos anuncios</Button>
+          </div>
+        </aside>
+      </StyleAnnoucePage>
+      <Footer />
+    </>
   );
 };
--- automax/src/pages/Announce/style.ts
+++ automax/src/pages/Announce/style.ts
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const StyleAnnoucePage = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0 12px;
-  gap: 17px;
+  .body {
+    padding: 40px 12px 0 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 17px;
+  }
 
   .backGround {
     width: 100vw;
@@ -32,6 +34,7 @@ export const StyleAnnoucePage = styled.div`
   }
 
   .divInfosCar {
+    width: 100%;
     padding: 44px 24px 28px 24px;
     background-color: var(--colors-grey-10);
     border-radius: 4px;
@@ -162,6 +165,13 @@ export const StyleAnnoucePage = styled.div`
     flex-direction: column;
     gap: 24px;
 
+    .divInputButton {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+    }
+
     input {
       height: 130px;
     }
@@ -177,5 +187,129 @@ export const StyleAnnoucePage = styled.div`
         color: var(--colors-grey-3);
       }
     }
+  }
+
+  .divAnnoucerDesck,
+  .divPhotosDesck {
+    display: none;
+  }
+
+  @media (min-width: 720px) {
+    display: flex;
+    justify-content: center;
+    gap: 46px;
+
+    .body {
+      max-width: 752px;
+      padding: 40px 0 0 0;
+    }
+
+    .divInfoNumbers {
+      flex-direction: row !important;
+      align-items: center;
+      justify-content: space-between;
+    }
+    .divPhotosDesck {
+      max-width: 440px;
+      padding: 36px 28px;
+      background-color: var(--colors-grey-10);
+      border-radius: 4px;
+      display: flex;
+      flex-direction: column;
+      gap: 30px;
+
+      div:first-child {
+        display: flex;
+      }
+
+      .divImgs {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        div {
+          display: flex;
+          gap: 10px;
+          div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 4px;
+            width: 33.33%;
+            height: 90px;
+            background-color: var(--colors-grey-5);
+            padding: 20px;
+          }
+        }
+        img {
+          width: 100%;
+        }
+      }
+    }
+
+    .divAnnoucerDesck {
+      max-width: 440px;
+      padding: 36px 28px;
+      background-color: var(--colors-grey-10);
+      border-radius: 4px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 28px;
+
+      .divTagName {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 28px;
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 77px;
+          height: 77px;
+          border-radius: 50%;
+          background-color: var(--colors-brand-1);
+          color: var(--colors-whiteFixed);
+        }
+      }
+
+      p {
+        line-height: 28px;
+        text-align: center;
+      }
+
+      button {
+        width: 80%;
+      }
+    }
+
+    .aside {
+      padding-top: 40px;
+      display: flex;
+      flex-direction: column;
+      gap: 34px;
+    }
+
+    .backGround {
+      height: 576px;
+    }
+
+    .divComment {
+      .divInputButton {
+        position: relative;
+      }
+
+      button {
+        position: absolute;
+        right: 11px;
+        bottom: 13px;
+      }
+    }
+  }
+
+  .divPhotos,
+  .divAnnoucer {
+    display: none;
   }
 `;
