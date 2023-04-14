c1e79dfca260f172be9f2f41ce892b58a4a98a57
M7uricio <m7uricio.dev@gmail.com>
2023-04-14 11:11:04 -0300

    feat: resolve merging

--- automax/src/routes/index.tsx
+++ automax/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import { Announce } from "../pages/Announce";
+import { Home } from "../pages/Home";
 
 const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/announce" element={<Announce />}></Route>
+      <Route path="/announce" element={<Announce />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 };
