98d122d68b1f79ddedfb71f0a1007b6c140ce87b
Lucas Bueno <gelanlucas@gmail.com>
2023-04-14 10:12:45 -0300

    feat: HomePage

--- automax/src/App.tsx
+++ automax/src/App.tsx
@@ -1,10 +1,12 @@
 
 import { GlobalStyle } from "./style/globalStyles";
 import Routes from "./routes";
+import { Home } from "./pages/Home";
 
 const App = () => {
   return (
     <>
+      <Home />
       <GlobalStyle />
       <Routes />
     </>
