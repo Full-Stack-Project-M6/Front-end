ae9d4a2a8c5d0d5026dacda1d820e6dd019a50d5
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-04-20 09:30:52 -0300

    feat: add new HTML for Profile page

--- automax/src/pages/ProfileView/index.tsx
+++ automax/src/pages/ProfileView/index.tsx
@@ -1,4 +1,6 @@
+import AnnouncerCard from "../../components/Cards/AnnouncerCard"
 import { NavBar } from "../../components/NavBar"
+import { Footer } from "../../components/footer"
 
 export const ProfileView = () => {
     return (
@@ -6,6 +8,70 @@ export const ProfileView = () => {
         <NavBar variant="logged"/>
         <section>
         </section>
+        <div>
+            <p>SL</p>
+            <div>
+                <h4>Samuel Leão</h4>
+                <button>Anunciante</button>
+            </div>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+            </p>
+            <div>
+                <button> Criar anuncio</button>
+            </div>
+        </div>
+        <section>
+            <ul>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+                <li>
+                    <AnnouncerCard/>
+                </li>
+            </ul>
+            <div>
+                <p>1 de 2</p>
+                <h2>Seguinte</h2>
+            </div>
+        </section>
+        <Footer/>
         </>
     )
 }
