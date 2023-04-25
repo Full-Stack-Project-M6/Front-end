bc384556ac21101a4db141ff7c46555aa1095fcb
M7uricio <m7uricio.dev@gmail.com>
2023-04-25 15:47:01 -0300

    feat: create component ListAnnounceUser

--- automax/src/components/Cards/AnnouncerCard/index.tsx
+++ automax/src/components/Cards/AnnouncerCard/index.tsx
@@ -2,12 +2,17 @@ import { StyledAnnouncerCard } from "./style";
 import Announcer from "../../Announcer";
 import { Body1, Body2 } from "../../Typography";
 import { annnounceMocked } from "../../../utils";
+import { IAnnounceCard } from "../../../interfaces/announce";
 
-const AnnouncerCard = () => {
+interface IAdCardProps {
+  data: IAnnounceCard;
+}
+
+const AnnouncerCard = (props: IAdCardProps) => {
   return (
     <StyledAnnouncerCard>
       <div className="divImg">
-        {annnounceMocked.active ? (
+        {props.data.published ? (
           <div className="active">
             <p>Ativo</p>
           </div>
@@ -17,22 +22,22 @@ const AnnouncerCard = () => {
           </div>
         )}
 
-        <img src={annnounceMocked.imgCar} alt="" />
+        <img src={props.data.image_cover} alt="" />
       </div>
       <div className="divInfos">
         <Body1 weight={600}>{annnounceMocked.title}</Body1>
-        <Body2>{annnounceMocked.description}</Body2>
+        <Body2>{props.data.description}</Body2>
       </div>
       <div className="divAnnouncer">
         <Announcer />
       </div>
       <div className="divKmAndPrice">
         <div>
-          <Body2 weight={500}>{annnounceMocked.km} KM</Body2>
+          <Body2 weight={500}>{props.data.kilometer} KM</Body2>
           <Body2 weight={500}>{annnounceMocked.year}</Body2>
         </div>
         <div>
-          <Body1 weight={600}>R$ {annnounceMocked.price}</Body1>
+          <Body1 weight={600}>R$ {props.data.price}</Body1>
         </div>
       </div>
     </StyledAnnouncerCard>
