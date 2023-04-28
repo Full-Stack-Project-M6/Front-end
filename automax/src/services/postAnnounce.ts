87473067174ae2dbe35f4c92b8306313f90420a5
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-04-28 11:47:46 -0300

    hotfix: change createAnnounce to announceContext file

--- automax/src/services/postAnnounce.ts
+++
@@ -1,8 +0,0 @@
-import { IAnnounce, ICreateAnnounce } from "../interfaces/announce"
-import {  instance } from "./apiKenzie"
-
-export async function postAnnounce(announceData:ICreateAnnounce): Promise<IAnnounce> {
-    const {data} = await instance.post<IAnnounce>(`/announce`, announceData)
-
-    return data
-}
