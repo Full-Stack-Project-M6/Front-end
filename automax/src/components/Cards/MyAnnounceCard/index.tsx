07ef59c3cb825055b1773cd9d60e3edfe508256a
M7uricio <m7uricio.dev@gmail.com>
2023-05-02 10:55:01 -0300

    fix: register user account type

--- automax/src/components/Cards/MyAnnounceCard/index.tsx
+++ automax/src/components/Cards/MyAnnounceCard/index.tsx
@@ -1,12 +1,12 @@
 import { StyledMyAnnounceCard } from "./style";
 import Announcer from "../../Announcer";
 import { Body1, Body2 } from "../../Typography";
-import { annnounceMocked } from "../../../utils";
 import { IAnnounceCard } from "../../../interfaces/announce";
 import { StyledButton } from "../../Button/styles";
 import { useContext } from "react";
 import { ModalContext } from "../../../context/modalContext";
 import { AnnounceContext } from "../../../context/announceContext";
+import { useNavigate } from "react-router-dom";
 
 interface IAdCardProps {
   data: IAnnounceCard;
@@ -15,6 +15,7 @@ interface IAdCardProps {
 const MyAnnounceCard = (props: IAdCardProps) => {
   const { formUpdateAnnounceSetOpen } = useContext(ModalContext);
   const { setAnnounce } = useContext(AnnounceContext);
+  const navigate = useNavigate();
 
   return (
     <StyledMyAnnounceCard>
@@ -22,7 +23,7 @@ const MyAnnounceCard = (props: IAdCardProps) => {
         <img src={props.data.image_cover} alt="" />
       </div>
       <div className="divInfos">
-        <Body1 weight={600}>{annnounceMocked.title}</Body1>
+        <Body1 weight={600}>{props.data.model.model}</Body1>
         <Body2>{props.data.description}</Body2>
       </div>
       <div className="divAnnouncer">
@@ -31,7 +32,7 @@ const MyAnnounceCard = (props: IAdCardProps) => {
       <div className="divKmAndPrice">
         <div>
           <Body2 weight={500}>{props.data.kilometer} KM</Body2>
-          <Body2 weight={500}>{annnounceMocked.year}</Body2>
+          <Body2 weight={500}>{props.data.year.year}</Body2>
         </div>
         <div>
           <Body1 weight={600}>R$ {props.data.price},00</Body1>
@@ -42,13 +43,22 @@ const MyAnnounceCard = (props: IAdCardProps) => {
           className="outline1"
           onClick={() => {
             setAnnounce(props.data);
-
             formUpdateAnnounceSetOpen(true);
           }}
         >
           Editar
         </StyledButton>
-        <StyledButton className="outline1">Ver detalhes</StyledButton>
+        <StyledButton
+          className="outline1"
+          onClick={() => {
+            // console.log(props.data);
+
+            setAnnounce(props.data);
+            navigate("/announce", { replace: true });
+          }}
+        >
+          Ver detalhes
+        </StyledButton>
       </div>
     </StyledMyAnnounceCard>
   );
